567f668a46340d27835df470f6027e2a07ab0c0b
StarLinkFans <ImeroO@dragoncore.ca>
2023-05-30 19:39:39 -0600

    first commit

---
+++ src/main/java/org/comit/course/_01_variables/hello.java
@@ -0,0 +1,19 @@
+package org.comit.course._01_variables;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = 5;
+		char char1 = '1';
+		float num5 = 5.2f;
+		String mark = "Then";
+		System.out.println(num);
+		System.out.println(char1);
+		System.out.println(num5);
+		System.out.println(mark);
+		
+	}
+
+}
